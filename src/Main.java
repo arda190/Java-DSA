caac418715a603f17d636a4221f52d8fc8f0da15
arda190 <aardaa_karakas@outlook.com>
2024-12-18 23:09:30 +0300

    BufferReader are changed to Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,51 @@
 import java.io.*;
 import java.io.*;
 import java.util.*;
-public class Main{
+public class Main {
 
-    public static int getCityNumber(String fileName) {
+    public static int getCityNumber(String fileName) throws Exception {
         int cityNumber = 0;
         String line;
-        try (BufferedReader myReader = new BufferedReader(new FileReader(fileName))) {
-            if ((line = myReader.readLine()) != null) {
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            if (scanner.hasNextLine()) {
+                line = scanner.nextLine();
                 cityNumber = Integer.parseInt(line);
             }
-        } catch (IOException e) {
-            System.out.println("File not found");
+        } catch (Exception e) {
+            throw new Exception();
         }
         return cityNumber;
     }
-    public static String[] getCities(String fileName) {
+
+    public static String[] getCities(String fileName) throws Exception {
         String[] cities = new String[getCityNumber(fileName)];
         String line;
-        int count=1;
-        try (BufferedReader myReader = new BufferedReader(new FileReader(fileName))) {
-            boolean flag=true;
-            while((line = myReader.readLine()) != null && flag) {
-                if(count==2){
-                    cities=line.split(",");
-                    flag=false;
+        int count = 1;
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            boolean flag = true;
+            while (scanner.hasNextLine() && flag) {
+                line = scanner.nextLine();
+                if (count == 2) {
+                    cities = line.split(",");
+                    flag = false;
                 }
                 count++;
             }
-        } catch (IOException e) {
-            System.out.println("File not found");
         }
         return cities;
     }
+
     public static int getRouteNumber(String fileName) {
         int route = 0;
         String line;
-        int count=1;
-        try (BufferedReader myReader = new BufferedReader(new FileReader(fileName))) {
-            boolean flag=true;
-            while((line = myReader.readLine()) != null && flag) {
-                if(count==3){
-                    route=Integer.parseInt(line);
-                    flag=false;
+        int count = 1;
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            boolean flag = true;
+            while (scanner.hasNextLine() && flag) {
+                line = scanner.nextLine();
+                if (count == 3) {
+                    route = Integer.parseInt(line);
+                    flag = false;
                 }
                 count++;
 
@@ -52,77 +55,74 @@ public class Main{
         }
         return route;
     }
+
     public static String[][] getRoutes(String fileName) {
-        int route=getRouteNumber(fileName);
+        int route = getRouteNumber(fileName);
         String[][] routes = new String[route][3];
         String line;
-        int count=1;
-        int index=0;
-        try (BufferedReader myReader = new BufferedReader(new FileReader(fileName))) {
-            boolean flag=true;
-            while((line = myReader.readLine()) != null && flag) {
-                if(count>=4 && count<(4+route)){
-                    routes[index][0]=line.split(" ")[0];
-                    routes[index][1]=line.split(" ")[1];
-                    routes[index][2]=line.split(" ")[2];
+        int count = 1;
+        int index = 0;
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            boolean flag = true;
+            while (scanner.hasNextLine() && flag) {
+                line = scanner.nextLine();
+                if (count >= 4 && count < (4 + route)) {
+                    routes[index][0] = line.split(" ")[0];
+                    routes[index][1] = line.split(" ")[1];
+                    routes[index][2] = line.split(" ")[2];
                     index++;
                     count++;
-                }
-                else if(count==(4+route)){
-                    flag=false;
-                }
-                else {
+                } else if (count == (4 + route)) {
+                    flag = false;
+                } else {
                     count++;
                 }
                 //return routes;
             }
-        } catch (IOException e) {
-            System.out.println("File not found");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
-
         return routes;
     }
+
     public static String[] getTarget(String fileName) throws FileNotFoundException {
         String line;
-        int count=1;
-        int targetLine=getRouteNumber(fileName)+4;
-        String[] target=new String[2];
-        try(BufferedReader myReader=new BufferedReader(new FileReader(fileName))){
-            boolean flag=true;
-            while((line=myReader.readLine())!=null && flag) {
-                if(count==targetLine){
-                    target=line.split(" ");
-                    flag=false;
-                }
-                else{
+        int count = 1;
+        int targetLine = getRouteNumber(fileName) + 4;
+        String[] target = new String[2];
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            boolean flag = true;
+            while (scanner.hasNextLine() && flag) {
+                line = scanner.nextLine();
+                if (count == targetLine) {
+                    target = line.split(" ");
+                    flag = false;
+                } else {
                     count++;
                 }
             }
 
-        } catch (IOException e) {
-            System.out.println("File not found");
         }
         return target;
     }
 
 
-
-
     public static boolean isInteger(String s) {
-        try{
+        try {
             Integer.parseInt(s.trim());
             return true;
-        }
-        catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
+
     public static boolean isLetter(String s) {
-        return s.length()==1 && Character.isLetter(s.charAt(0));
+        return s.length() == 1 && Character.isLetter(s.charAt(0));
     }
-    public static boolean areTheyLetter(String[]s){
-        for(String a:s){
-            if(!isLetter(a.trim())){
+
+    public static boolean areTheyLetter(String[] s) {
+        for (String a : s) {
+            if (!isLetter(a.trim())) {
                 return false;
             }
         }
@@ -130,23 +130,23 @@ public class Main{
     }
 
 
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         //String filename=args[0];
-        String filename = "C:\\Users\\90506\\OneDrive\\Masaüstü\\Proje\\Map23.txt";
-        //filename="Map100.txt";
-        //filename=args[0];
+        String filename = "C:\\Users\\90506\\OneDrive\\Masaüstü\\Proje\\Map2.txt";
+        //String filename=args[0];
         int lineNumber = 0;
         //int cityNumber = getCityNumber(filename);
-        int cityNumber=0;
+        int cityNumber = 0;
         try (Scanner sc = new Scanner(new File(filename))) {
-            while (sc.hasNext()) {
+            while (sc.hasNextLine()) {
                 sc.nextLine();
                 lineNumber++;
             }
-        } catch (FileNotFoundException e) {
+            System.out.println(lineNumber);
+        }
+        catch (Exception e) {
+            System.out.println("File not found");
             System.exit(0);
-
         }
 
         String line;
@@ -155,20 +155,19 @@ public class Main{
         int errorLine = 1;
         int index = 0;
         int route = 0;
-        //BufferedReader myReader = new BufferedReader(new FileReader(filename))
-        try (BufferedReader myReader = new BufferedReader(new FileReader(filename))) {
-            if ((line = myReader.readLine()) != null) {
-                if (!isInteger(line)||Integer.parseInt(line)<=0) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            if (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                if (!isInteger(line) || Integer.parseInt(line) <= 0) {
                     errors[index] = "<" + errorLine + ">" + "First line must be a positive integer representing the number of cities.";
                     isError = true;
                     index++;
                 }
-
             }
-
             errorLine++;
-            if ((line = myReader.readLine()) != null && !isError) {
+            if (scanner.hasNextLine() && !isError) {
                 cityNumber = getCityNumber(filename);
+                line=scanner.nextLine();
                 String[] parts = line.split(" ");
                 if (parts.length != cityNumber) {
                     errors[index] = "<" + errorLine + ">" + "There should be " + cityNumber + " cities, but there are " + parts.length + " city are listed.";
@@ -187,7 +186,8 @@ public class Main{
             }
 
             errorLine++;
-            if ((line = myReader.readLine()) != null) {
+            if ((scanner.hasNextLine())) {
+                line = scanner.nextLine();
                 if (!isInteger(line)) {
                     errors[index] = "<" + errorLine + ">" + "Number of routes must be a positive integer.";
                     isError = true;
@@ -199,11 +199,32 @@ public class Main{
 
             errorLine++;
             boolean flag = true;
-            int numberOfRoutes = getRouteNumber(filename);
             int start = errorLine;
             for (int i = 0; i < lineNumber - 4; i++) {
-                if ((line = myReader.readLine()) != null) {
+                if (scanner.hasNextLine()) {
+                    line = scanner.nextLine();
                     String[] parts = line.split(" ");
+                    if(parts.length<3){
+                        errors[index] = "<" + errorLine + ">" + "It must be this format <City City Time>.";
+                        isError = true;
+                        index++;
+                        continue;
+                    }
+                    boolean isMismatch = true;
+                    for(i=0;i<getCityNumber(filename);i++){
+                        String a=getCities(filename)[0].split(" ")[i];
+                        System.out.println(a);
+                        if(a.equals(parts[0])|| a.equals(parts[1])){
+                            isMismatch = false;
+                            break;
+                        }
+                    }
+                    if(!isMismatch){
+                        errors[index] = "<" + errorLine + ">" + "City name is not match.>";
+                        isError = true;
+                        index++;
+                    }
+
                     if (!(isLetter(parts[0]) && isLetter(parts[1]) && isInteger(parts[2]))) {
                         errors[index] = "<" + errorLine + ">" + "It must be this format <City City Time>.";
                         isError = true;
@@ -212,23 +233,27 @@ public class Main{
                     errorLine++;
                 }
             }
+            int numberOfRoutes =0;
             int currentRouteNumber = errorLine - start;
-            if (numberOfRoutes != 0) {
-                if (currentRouteNumber != numberOfRoutes) {
-                    errors[index] = "<" + "4-" + (errorLine - 1) + ">" + "There should be " + numberOfRoutes + " routes, but there are " + currentRouteNumber + " routes.";
-                    isError = true;
-                    index++;
+            try{
+                numberOfRoutes=getRouteNumber(filename);
+                if (numberOfRoutes != 0) {
+                    if (currentRouteNumber != numberOfRoutes) {
+                        errors[index] = "<" + "4-" + (errorLine - 1) + ">" + "There should be " + numberOfRoutes + " routes, but there are " + currentRouteNumber + " routes.";
+                        isError = true;
+                        index++;
+                    }
                 }
             }
-
-            if ((line = myReader.readLine()) != null) {
+            catch (Exception ignored) {
+            }
+            if (scanner.hasNextLine()) {
+                line = scanner.nextLine();
                 String[] parts = line.split(" ");
                 if (!(isLetter(parts[0]) && isLetter(parts[1]))) {
                     errors[index] = "<" + errorLine + ">" + "It must be this format <City City>.";
                 }
             }
-        } catch (IOException e) {
-            System.out.println("File not found");
         }
         if (isError) {
             for (String a : errors) {
@@ -239,22 +264,22 @@ public class Main{
         } else {
             System.out.println("File read is successful!");
 
-            String[]cityList=getCities(filename)[0].split(" ");
+            String[] cityList = getCities(filename)[0].split(" ");
 
-            City[] cities=new City[cityNumber];
-            for(int i=0;i<cityNumber;i++){
-                cities[i]=new City(cityList[i],i);
+            City[] cities = new City[cityNumber];
+            for (int i = 0; i < cityNumber; i++) {
+                cities[i] = new City(cityList[i], i);
             }
 
 
-            CountryMap map=new CountryMap(cities,getRoutes(filename),getRouteNumber(filename));
-            WayFinder wf=new WayFinder(map);
-            String[] d=getTarget(filename);
-            String start=d[0];
-            String end=d[1];
-            String result=wf.findFastestWay(start,end);
+            CountryMap map = new CountryMap(cities, getRoutes(filename), getRouteNumber(filename));
+            WayFinder wf = new WayFinder(map);
+            String[] d = getTarget(filename);
+            String start = d[0];
+            String end = d[1];
+            String result = wf.findFastestWay(start, end);
 
-            if(result!=null) {
+            if (result != null) {
 
                 Formatter f = null;
                 try {
@@ -270,9 +295,6 @@ public class Main{
                 }
             }
         }
-
     }
-
 }
-
 
