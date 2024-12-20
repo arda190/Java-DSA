85edd9948bb25f213093c215e898a471130b4ee7
arda190 <aardaa_karakas@outlook.com>
2024-12-20 22:07:04 +0300

    Some methods are added and some bugs fixed

--- src/CountryMap.java
+++ src/CountryMap.java
@@ -1,27 +1,44 @@
 public class CountryMap {
-    private City[] cities;
-    private String[][] routes;
-    public CountryMap(City[] cities, String[][] routes) {
-        this.cities = cities;
-        this.routes = routes;
+    City[] cities;
+    String[][] routes;
+    int routeNumber;
+    public CountryMap(City[] cities,String[][] routes,int routeNumber) {
+        this.cities = new City[cities.length];
+        for(int i = 0; i < cities.length; i++){
+            this.cities[i] = cities[i];
+        }
+        this.routes=new String[routes.length][3];
+        for(int i = 0; i < routes.length; i++){
+            this.routes[i]=routes[i];
+        }
+        this.routeNumber=routeNumber;
     }
     public City[] getCities() {
+
         return cities;
     }
-    public String[][] getRoutes() {
-        return routes;
-    }
-    public String[] getRoutes(String city) {
-        String[]cityRoutes = new String[routes.length];
-        int index=0;
-        for(String[] route : routes) {
-            if(route[0].equals(city)) {
-                cityRoutes[index] = route[1]+" "+route[2];
-                index++;
+    public String[] getRoutes(String name) {
+        int count = 0;
+        for (int i = 0; i < routeNumber; i++) {
+            if(routes[i][0]!=null && routes[i][1]!=null) {
+                if (routes[i][0].equals(name) || routes[i][1].equals(name)) {
+                    count++;
+                }
             }
         }
-        return cityRoutes;
+        String[] route = new String[count];
+        int index = 0;
+        for (int i = 0; i <routeNumber ; i++) {
+            if(routes[i][0]!=null && routes[i][1]!=null){
+                if (routes[i][0].equals(name)) {
+                    route[index] = routes[i][1] + " " + routes[i][2];
+                    index++;
+                } else if (routes[i][1].equals(name)) {
+                    route[index] = routes[i][0] + " " + routes[i][2];
+                    index++;
+                }
+            }
+        }
+        return route;
     }
-
-
 }
