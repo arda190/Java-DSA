a336b83852f81930737a08fef1d8bd0e59e6d520
arda190 <aardaa_karakas@outlook.com>
2025-07-01 11:34:43 +0300

    Method which return max subarray are added

--- demo3/DataStructures/src/KadanesAlgorithm.java
+++ demo3/DataStructures/src/KadanesAlgorithm.java
@@ -8,10 +8,45 @@ public class KadanesAlgorithm {
             maxSoFar=Math.max(maxSoFar,maxEndingHere);
         }
       System.out.println(maxSoFar);
+    }
+
+    public int[] Kadane(int[]array){
+        int maxSoFar=array[0];
+        int maxEndingHere = array[0];
+        int start=0;
+        int end=0;
+        int tempStart=0;
+        for(int i = 1; i < array.length; i++) {
+            if(maxEndingHere+array[i]<array[i]) {
+                maxEndingHere=array[i];
+                tempStart=i;
+            }
+            else if(maxEndingHere+array[i]>array[i]){
+                maxEndingHere=maxEndingHere+array[i];
+
+            }
+            if(maxSoFar<maxEndingHere) {
+                maxSoFar=maxEndingHere;
+                start=tempStart;
+                end=i;
+            }
+        }
+        int[] newArray=new int[end-start+1];
+        int j=0;
+        for(int i = start; i <= end; i++) {
+            newArray[j]=array[i];
+            j++;
+        }
+        return newArray;
 
     }
+
+
+
     public static void main(String[] args) {
         KadanesAlgorithm kadanesAlgorithm = new KadanesAlgorithm();
         kadanesAlgorithm.Kadanes(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        int[] result = kadanesAlgorithm.Kadane(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+        System.out.println(java.util.Arrays.toString(result));
     }
 }
