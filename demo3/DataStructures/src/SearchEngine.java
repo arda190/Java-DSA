b40a66ec179640ab6130160d37397cc00d680e9e
arda190 <aardaa_karakas@outlook.com>
2025-07-02 15:01:39 +0300

    Refactor SearchEngine to preserve newlines and highlight punctuation correctly

--- demo3/DataStructures/src/SearchEngine.java
+++ demo3/DataStructures/src/SearchEngine.java
@@ -1,45 +1,44 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class SearchEngine {
     public static void searchText(String file,String target) throws FileNotFoundException {
-        StringBuilder sb=new StringBuilder();
+        StringBuilder result=new StringBuilder();
         String line=null;
         try(BufferedReader bf=new BufferedReader(new FileReader(file))){
             while((line=bf.readLine())!=null){
-                sb.append(line);
-                sb.append("\n");
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        String text=sb.toString();
-        String[] words=text.split("\\s+");
-        StringBuilder result=new StringBuilder();
-        for(int i=0;i<words.length;i++){
-                String clean=words[i].replaceAll("\\p{Punct}","").toLowerCase().trim();
-                if(clean.isEmpty()) continue;
-                if (clean.equalsIgnoreCase(target)) {
+
+                String[] words=line.split("\\s+");
+
+                for(int i=0;i<words.length;i++){
+                 String clean=words[i].replaceAll("\\p{Punct}","").toLowerCase().trim();
+                 if(clean.isEmpty()) continue;
+                 if (clean.equalsIgnoreCase(target)) {
                     result.append(" ");
                     result.append("<<" + words[i] + ">>");
-                } else {
+                 } else {
                     result.append(" ");
                     result.append(words[i]);
-                }
-            }
-        result.append("\n");
-
-        System.out.println(result);
+                 }
+             }
+                result.append("\n");
 
         }
+            System.out.println(result);
+
+        }catch (IOException e){
+          e.printStackTrace();
+        }
+    }
 
 
 
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        searchText("filePath","test");
+        searchText("filePath.txt","frequency");
 
 
     }
